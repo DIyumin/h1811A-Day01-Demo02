0446a02c820905a07f44be30229bc4ccf3eefbe9
d20020113 <1637404370@qq.com>
2019-05-28 16:56:57 +0800

    Initial commit

---
+++ src/com/jiyun/Aservlet.java
@@ -0,0 +1,26 @@
+package com.jiyun;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author shkstart
+ * @create 2019-05-28 11:34
+ */
+public class Aservlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.getWriter().write("hellow Servlet");
+        resp.getWriter().write("邸玉民");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
